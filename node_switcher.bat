b7acb739732e815d769153afd2c21efbbc4c688b
Ravi Kanth <srkt@users.noreply.github.com>
2024-06-21 17:31:11 -0400

    Create node_switcher.bat

---
+++ node_switcher.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal
+
+REM Set the folder where different Node.js versions are stored
+set "nodeVersionsFolder=C:\path\to\node\versions"
+
+REM List available Node.js versions
+echo Available Node.js versions:
+for /d %%d in ("%nodeVersionsFolder%\*") do (
+    echo %%~nxd
+)
+
+REM Prompt the user to select a Node.js version
+set /p nodeVersion="Enter the Node.js version you want to use: "
+
+REM Check if the selected version exists
+if not exist "%nodeVersionsFolder%\%nodeVersion%" (
+    echo The specified Node.js version does not exist.
+    goto end
+)
+
+REM Set the selected Node.js version to the PATH
+set "nodePath=%nodeVersionsFolder%\%nodeVersion%\bin"
+set "PATH=%nodePath%;%PATH%"
+
+REM Verify the change
+echo Switched to Node.js version:
+node -v
+
+:end
+endlocal
+pause
